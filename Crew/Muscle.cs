3bfaa32ecb79b0927c3318485be815c96d231331
jenniferstarkey <jenniferrstarkey@yahoo.com>
2020-12-02 15:56:54 -0600

    printing out a list or team members

--- Crew/Muscle.cs
+++ Crew/Muscle.cs
@@ -7,6 +7,7 @@ namespace HeistPart2
         public string Name { get; set; }
         public int SkillLevel { get; set; }
         public int PercentageCut { get; set; }
+        public string Specialty { get; set; }
 
         public void PerformSkill(Bank bank)
         {
