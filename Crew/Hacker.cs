f6b8cf37592a99f593eae2318005581cd925b37f
joseph tidwell <joseph.tidwell09@gmail.com>
2020-12-02 11:13:28 -0600

    added all robber persons

---
+++ Crew/Hacker.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace HeistPart2
+{
+    public class Hacker : IRobber
+    {
+        public string Name { get; set; }
+        public int SkillLevel { get; set; }
+        public int PercentageCut { get; set; }
+
+        public void PerformSkill(Bank bank)
+        {
+            int NewScore = (bank.AlarmScore - SkillLevel);
+            Console.WriteLine($"{Name} is hacking the alarm system. Decreased alarm score {SkillLevel} points");
+            if (NewScore <= 0)
+            {
+                Console.WriteLine($"{Name} has turned off the alarms!");
+            }
+        }
+    }
+}
